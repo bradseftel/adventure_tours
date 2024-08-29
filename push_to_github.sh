aa0a2d351e6288d50a426e0e0892768a142c5a44
Brad Seftel <177548123+bradseftel@users.noreply.github.com>
2024-08-29 12:13:35 -0230

    Automated Git Commit using Bash Script

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
+
 # Variables
-REPO_NAME="adventure_tours"
-GITHUB_USERNAME="bradseftel"
-BRANCH_NAME="main"  # Change this if your branch name is different
 COMMIT_MESSAGE="Automated Git Commit using Bash Script"  # You can customize this
 
 #Step 1: Add all files to the staging area
@@ -12,9 +10,6 @@ git add .
 # Step 2: Commit the files
 git commit -m "$COMMIT_MESSAGE"
 
-# Step 3: Add remote repository
-#git remote add origin https://github.com/$GITHUB_USERNAME/$REPO_NAME.git
-
-# Step 4: Push to GitHub
+# Step 3: Push to GitHub
 git push
-echo "Pushed to https://github.com/$GITHUB_USERNAME/$REPO_NAME.git on branch $BRANCH_NAME"
+
